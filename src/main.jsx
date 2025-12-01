1338a64c0e22978517d3ccdb390228ffa5da60b3
danil <daniltop.biz@gmail.com>
2025-12-01 19:34:18 +0700

    fix(router): use HashRouter so refreshes won't 404 on static hosting

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import { QuizHistoryProvider } from "./context/QuizHistory.jsx";
@@ -9,7 +9,7 @@ import { ThemeProvider } from "./context/ThemeContext.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <Router>
       <ThemeProvider>
         <QuizHistoryProvider>
           <SettingsProvider>
@@ -17,6 +17,6 @@ createRoot(document.getElementById("root")).render(
           </SettingsProvider>
         </QuizHistoryProvider>
       </ThemeProvider>
-    </BrowserRouter>
+  </Router>
   </StrictMode>
 );
